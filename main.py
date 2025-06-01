fa9bedb61acfba615a02b01969e8bd80fc3cdb64
joudar11 <krystofklika@pm.me>
2025-06-01 11:49:03 +0200

    rename file

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ from datetime import datetime, timedelta
 divider = "=" * 30
 # --- ÚDAJE K VYPLNĚNÍ ---
 JMENO = "Kryštof Klika"
-CISLO_DOKLADU = None
-CLENSKE_ID = None  # nebo None, pokud nemáš
+CISLO_DOKLADU = "ZP262518"
+CLENSKE_ID = "VZGxUDEf"  # nebo None, pokud nemáš
 DIVIZE = "Pistole"  # bude vybráno v dropdownu
 URL = "https://www.loslex.cz/contest/353"
-LOGIN = None
-HESLO = None
+LOGIN = "krystofklika@pm.me"
+HESLO = "E9JESU(na;TAC:TC<r93"
 # DATUM_CAS_REGISTRACE = None 
 DATUM_CAS_REGISTRACE ="2025-06-02 10:00:00"
 SQUAD = "1"
