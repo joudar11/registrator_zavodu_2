2b996423664327790567d7ab7c9ff914d59d465b
joudar11 <krystofklika@pm.me>
2025-06-04 13:51:13 +0200

    reduntant variable

--- main.py
+++ main.py
@@ -25,10 +25,9 @@ nazev_zavodu = None
 
 FATAL_ERROR = False
 
-SQUAD = str(SQUAD)
 REG_URL = "https://www.loslex.cz/contest/registration"
 DIVIZE_local = DIVIZE # Bere si divizi do proměnné, se kterou je možné v rámci main dále pracovat a měnit ji (pro ochranu proti neexistující  divizi)
-SQUAD_local = SQUAD
+SQUAD_local = str(SQUAD)
 POKUS_TIME = None # Čas zahájení pokusu o registraci (pro log)
 
 SELECTOR_TLACITKO_PRIHLASIT = r"body > div.min-h-screen.bg-gray-100.dark\:bg-gray-900 > nav > div.max-w-7xl.mx-auto.px-4.md\:px-6.lg\:px-8 > div > div.hidden.space-x-1.items-center.md\:-my-px.md\:ml-10.md\:flex > button.inline-flex.items-center.px-1.border-b-2.border-transparent.text-sm.font-medium.leading-5.text-gray-500.dark\:text-gray-400.hover\:text-gray-700.dark\:hover\:text-gray-300.hover\:border-gray-300.dark\:hover\:border-gray-700.focus\:outline-none.focus\:text-gray-700.dark\:focus\:text-gray-300.focus\:border-gray-300.dark\:focus\:border-gray-700.transition.duration-150.ease-in-out"  # tlačítko pro zobrazení login formuláře
