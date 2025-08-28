b5d2cef5c8602cc172d1d32a4af5f97744a4d798
joudar11 <krystofklika@pm.me>
2025-08-28 19:32:20 +0200

    edit gitignore, add data sample

---
+++ data_sample.py
@@ -0,0 +1,38 @@
+JMENO = "Jan Novák"
+# Celé jméno závodníka
+CISLO_DOKLADU = "AL123456"
+# Číslo ZP
+CLENSKE_ID = "ABcdEfGh"
+# LEX ID (volitelné - pokud nemáš, napiš None)
+DIVIZE = "Pistole"
+# Název divize přesně dle výběru na webu
+URL = "https://www.loslex.cz/contest/292"
+# URL konkrétního závodu
+LOGIN = "jan.novak"
+# Email pro přihlášení i notifikace
+HESLO = "tajneheslo123"
+# Heslo do systému LOS Lex
+DATUM_CAS_REGISTRACE = "2025-06-15 20:00:00"
+# Datum otevření registrace. Formát musí být RRRR-MM-DD HH:MM:SS
+SQUAD = 1
+# Číslo squadu, nebo r (v uvozovkách pro squad ROZHODČÍ)
+EMAIL_P = "tajneheslo123"
+# Proton Bridge heslo pro SMTP
+EMAIL_U = "jan.novak@pm.me"
+# Proton Bridge username odesílatele
+MZ = False
+# Mimo závod
+ZACATECNIK = False
+# Začátečník
+STAVITEL = False
+# Stavitel
+ROZHODCI = False
+# Rozhodčí
+POZNAMKA = "..."
+# Poznámka (volitelné) - Může být None
+PRITELKYNE = "jana.novakova@pm.me"
+# Email přítelkyně (None, pokud jsi single)
+JMENO_PRITELKYNE = "Jana"
+# Křestní jméno přítelkyně v prvním pádu
+RANDOM_WAIT = False
+# Zda má skript před odesláním registrace čekat náhodný počet sekund mezi 2 a 3, aby registrace vypadala věrohodněji
