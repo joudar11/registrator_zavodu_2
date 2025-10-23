76aef480320f455375e412907c314dc1ae3834dd
joudar11 <krystofklika@pm.me>
2025-10-23 20:43:12 +0200

    update data sample

--- data_sample.py
+++ data_sample.py
@@ -4,8 +4,10 @@ CISLO_DOKLADU = "AL123456"
 # Číslo ZP
 CLENSKE_ID = "ABcdEfGh"
 # LEX ID (volitelné - pokud nemáš, napiš None)
+# CLENSKE_ID = None
 DIVIZE = "Pistole"
-# Název divize PŘESNĚ dle výběru na webu
+# Název divize přesně dle výběru na webu (v drop down menu u registrace)
+# DIVIZE = "Optik/Pistole"
 URL = "https://www.loslex.cz/contest/292"
 # URL konkrétního závodu
 LOGIN = "jan.novak"
@@ -17,9 +19,9 @@ DATUM_CAS_REGISTRACE = "2025-06-15 20:00:00"
 SQUAD = 1
 # Číslo squadu, nebo r (v uvozovkách pro squad ROZHODČÍ)
 EMAIL_P = "tajneheslo123"
-# Proton Bridge heslo pro SMTP
+# Proton Bridge nebo Gmail heslo pro SMTP
 EMAIL_U = "jan.novak@pm.me"
-# Proton Bridge username odesílatele
+# Proton Bridge nebo Gmail username odesílatele
 MZ = False
 # Mimo závod
 ZACATECNIK = False
@@ -30,13 +32,16 @@ ROZHODCI = False
 # Rozhodčí
 POZNAMKA = "..."
 # Poznámka (volitelné) - Může být None
+# POZNAMKA = None
 PRITELKYNE = "jana.novakova@pm.me"
 # Email přítelkyně (None, pokud jsi single)
+# PRITELKYNE = None
 JMENO_PRITELKYNE = "Jana"
 # Křestní jméno přítelkyně v prvním pádu
+# Pokud je přítelkyně None, nepoužívá se
 RANDOM_WAIT = False
 # Zda má skript před odesláním registrace čekat náhodný počet sekund mezi 2 a 3, aby registrace vypadala věrohodněji
 INTERVAL = 1800
-# V jakém intervalu v sekundách se má kontrolovat volné místo na plném závodě (v plny_zavod.py)
+# V jakém intervalu v sekundách se má kontrolovat volné místo na plném závodě (s tímto údajem se pracuje pouze v plny_zavod.py)
 EMAIL_PROVIDER = "PROTON"
 # Poskytovatel emailových služeb. Možnosti jsou buď "PROTON" (S nainstalovaným Proton Bridge) nebo "GMAIL" (S specifickým Google apps password)
