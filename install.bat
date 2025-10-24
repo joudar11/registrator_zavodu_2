ad48bd52a9039d500185971c4f484c59da1785c5
Kryštof Klika <krystofklika@pm.me>
2025-10-24 12:25:25 +0200

    Update install.bat

--- install.bat
+++ install.bat
@@ -2,6 +2,6 @@ cd %appdata%
 git clone https://github.com/joudar11/registrator_zavodu_2
 cd ./registrator_zavodu_2
 python -m venv .venv
-.venv\Scripts\Activate.ps1
+. venv\Scripts\Activate.ps1
 pip install -r requirements.txt
 playwright install
