28048bde5bd0ea1db9e06db0a3b653a580cdc83c
Kryštof Klika <krystofklika@pm.me>
2025-10-24 12:24:29 +0200

    Create install.bat
    added install script

---
+++ install.bat
@@ -0,0 +1,7 @@
+cd %appdata%
+git clone https://github.com/joudar11/registrator_zavodu_2
+cd ./registrator_zavodu_2
+python -m venv .venv
+.venv\Scripts\Activate.ps1
+pip install -r requirements.txt
+playwright install
